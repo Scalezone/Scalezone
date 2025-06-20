eab0848462d8490d4cc46a10df26433e219e1f7a
Kerolos Amiel <kerolosamiel4@gmail.com>
2025-06-20 11:38:50 +0300

    refactor: modify toggleActiveClass function to take an arg and set it by def value 'active' & add comments

--- scripts/public/public.js
+++ scripts/public/public.js
@@ -1,10 +1,14 @@
+// Select the service dropdown elements
 const servicesLink = document.querySelector(".services-link");
 const servicesDropdown = document.querySelector(".services-menu");
 
+// Add a click event listener to the services link
 servicesLink.addEventListener("click", () => {
+  // Toggle the 'active' class on the dropdown menu
   toggleActiveClass(servicesDropdown);
 });
 
-function toggleActiveClass(ele) {
-  ele.classList.toggle("active");
+// Function to toggle the 'active' class on a given element
+function toggleActiveClass(ele, className = "active") {
+  ele.classList.toggle(className);
 }
