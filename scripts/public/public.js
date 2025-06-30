28878cbdf9518853c24798e80f383d2472213e78
Kerolos Amiel <kerolosamiel4@gmail.com>
2025-06-30 23:33:39 +0300

    refactor: Make some updates: => Add Resources dropdown in the top navigation => Add 'Blogs , Videos, Podcast' items => Add Language Switcher (EN/AR) => Reduce logo size and text in top bar => Make the header sticky on scroll

--- scripts/public/public.js
+++ scripts/public/public.js
@@ -1,15 +1,31 @@
 // DOM Elements - Navigation Components
 const servicesDropdownTrigger = document.querySelector(".services-link");
 const servicesDropdownMenu = document.querySelector(".services-menu");
+const resourcesDropdownTrigger = document.querySelector(".resources-link");
+const resourcesDropdownMenu = document.querySelector(".resources-menu");
 const mobileMenuToggle = document.querySelector(".hamburger");
 const mobileNavigationMenu = document.querySelector(".navbar");
+const navigationBar = document.querySelector(".header-container .bottom");
+const headerContainer = document.querySelector(".header-container");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > headerContainer.offsetHeight) {
+    addElementVisiblity(navigationBar, "scroll");
+  } else {
+    removeElementVisiblity(navigationBar, "scroll");
+  }
+});
 
 /**
  * Toggles the visibility of services dropdown menu
- * Handles the click event on the services link to show/hide the dropdown
+ * Handles the click event on the services and resources links to show/hide the dropdown
  */
 servicesDropdownTrigger.addEventListener("click", () => {
   toggleElementVisibility(servicesDropdownMenu);
+});
+
+resourcesDropdownTrigger.addEventListener("click", () => {
+  toggleElementVisibility(resourcesDropdownMenu);
 });
 
 /**
@@ -28,4 +44,16 @@ mobileMenuToggle.addEventListener("click", () => {
  */
 function toggleElementVisibility(element, className = "active") {
   element.classList.toggle(className);
+}
+
+function addElementVisiblity(element, className = "active") {
+  if (!element.classList.contains(className)) {
+    element.classList.add(className);
+  }
+}
+
+function removeElementVisiblity(element, className = "active") {
+  if (element.classList.contains(className)) {
+    element.classList.remove(className);
+  }
 }
