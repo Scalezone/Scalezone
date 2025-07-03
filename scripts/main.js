85eb07f482346bfe8bc8c8077c3b8527cb2e8bbf
Kerolos Amiel <kerolosamiel4@gmail.com>
2025-07-03 16:20:49 +0300

    Add Client Reviews Section below 'What Makes Us Different'

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,31 @@
-/**
- * @file This file is intended for use with index.html only.
- */
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  loop: true,
+  centerSlide: true,
+  fade: true,
+  grabCursor: true,
+  autoplay: true,
+  speed: 1000,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    780: {
+      slidesPerView: 2,
+    },
+    950: {
+      slidesPerView: 3,
+    },
+  },
+});
